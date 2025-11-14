9ee02eb5787933ba4f838030cd979c03c5ad421c
Gabriel Melo <gblsmlo@gmail.com>
2025-11-13 21:51:21 -0300

    fix(button): correct transition color and update outline variant background

--- src/components/ui/button/button.variants.ts
+++ src/components/ui/button/button.variants.ts
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 export type ButtonProps = VariantProps<typeof buttonVariants>
 
 export const buttonVariants = cva(
-	'inline-flex items-center justify-center gap-2 transition-color ease-linear duration-75 whitespace-nowrap rounded-sm text-sm font-semibold disabled:pointer-events-none disabled:bg-slate-300 disabled:border-slate-300 disabled:text-slate-500 [&_svg]:pointer-events-none [&_svg:not([class*="size-"])]:size-4 [&_svg]:shrink-0 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 focus-visible:ring-4 focus-visible:outline-1 aria-invalid:focus-visible:ring-0',
+	'inline-flex items-center justify-center gap-2 transition-colors ease-linear duration-75 whitespace-nowrap rounded-sm text-sm font-semibold disabled:pointer-events-none disabled:bg-slate-300 disabled:border-slate-300 disabled:text-slate-500 [&_svg]:pointer-events-none [&_svg:not([class*="size-"])]:size-4 [&_svg]:shrink-0 ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 focus-visible:ring-4 focus-visible:outline-1 aria-invalid:focus-visible:ring-0',
 	{
 		defaultVariants: {
 			size: 'base',
@@ -19,7 +19,7 @@ export const buttonVariants = cva(
 				destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
 				ghost: 'text-accent hover:bg-accent/10 hover:text-accent',
 				link: 'text-accent underline-offset-4 hover:underline',
-				outline: 'border border-accent/20 bg-transparent hover:bg-accent/10 text-accent',
+				outline: 'border border-accent/20 bg-accent/10 hover:bg-accent/20 text-accent',
 				solid: 'bg-accent text-accent-foreground hover:bg-accent/80',
 			},
 		},
